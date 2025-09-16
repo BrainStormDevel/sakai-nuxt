c2986d9c06b1ee7526b2e89bde5e030935e67cff
unknown <davidebonsangue@gmail.com>
2025-09-16 10:16:08 +0200

    Small fix

--- src/plugins/primevue.js
+++
@@ -1,9 +0,0 @@
-import { defineNuxtPlugin } from '#app'
-import PrimeVue from 'primevue/config'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(PrimeVue, {
-    theme: 'none', // We'll manage themes through CSS imports
-    ripple: true
-  })
-})
